bfef552c8dc974277d592fd49ffdc3191d8fb213
stdreg <stefandreger@protonmail.com>
2021-11-22 08:35:40 +0100

    adds max bullets and removes old ones

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,15 +21,15 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
-
+    
     def run_game(self):
         """main loop for the game"""
         while True: 
             self._check_events()     
             self.ship.update()  
-            self.bullets.update()     
-            self._update_screen()
-
+            self._update_bullets()            
+            self._update_screen()            
+                     
     def _check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -56,8 +56,16 @@ class AlienInvasion:
             self.ship.moving_left = False                                                                
 
     def _fire_bullet(self):
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        self.bullets.update()     
+        #remove old Bullets
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)                       
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
