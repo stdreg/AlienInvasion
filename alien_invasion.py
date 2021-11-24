ef0806d394e08fac20bc23ca1150faaa2aed72d8
stdreg <stefandreger@protonmail.com>
2021-11-24 09:11:58 +0100

    adds the first alien ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """Manage game assets and behavior"""
@@ -21,6 +22,8 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+        self._create_fleet()
     
     def run_game(self):
         """main loop for the game"""
@@ -72,8 +75,14 @@ class AlienInvasion:
         self.ship.blitme()
         for bullet in self.bullets:
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
+        
         #make most recent draw screen visible
         pygame.display.flip()
+
+    def _create_fleet(self):
+        alien = Alien(self)
+        self.aliens.add(alien)
 
 
 
