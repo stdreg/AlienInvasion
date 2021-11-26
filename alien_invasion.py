79f092e54f1ce829e1ca7d7cf584aa76dc713f2c
stdreg <stefandreger@protonmail.com>
2021-11-26 10:15:30 +0100

    creates an alien fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -76,15 +76,34 @@ class AlienInvasion:
         for bullet in self.bullets:
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
-        
+
         #make most recent draw screen visible
         pygame.display.flip()
 
     def _create_fleet(self):
+        # create an alien an find the number of aliens, that fit in one row
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
 
+        # determinate number of rows that fit on screen
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_heigth - (3*alien_height) -ship_height)
+        number_rows = available_space_y // (2 * alien_height)
 
+        # create the fleet of aliens
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                alien = self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+        self.aliens.add(alien)        
 
 if __name__ == "__main__":
     ai = AlienInvasion()
